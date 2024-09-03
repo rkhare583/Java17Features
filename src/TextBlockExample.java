0bb6f77b553ab3677c8c5a55e26d5e7700cdf458
rkhare007 <rkhare580@gmail.com>
2024-09-03 17:13:09 +0530

    example

---
+++ src/TextBlockExample.java
@@ -0,0 +1,10 @@
+public class TextBlockExample {
+    public static void main(String[] args) {
+        String name= """
+                My name
+                is
+                Rohit Khare
+                """;
+        System.out.println(name);
+    }
+}
