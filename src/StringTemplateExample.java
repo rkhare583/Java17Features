555b507c453c91e695141507b6847e3a90b087b4
rkhare007 <rkhare580@gmail.com>
2024-09-04 18:07:53 +0530

    example

---
+++ src/StringTemplateExample.java
@@ -0,0 +1,9 @@
+public class StringTemplateExample {
+    public static void main(String[] args) {
+        //This is ex of Java 21
+        int a=8;
+        int b=9;
+        String result = STR."\{a} + \{b} = \{a+b}";
+        System.out.println(result);
+    }
+}
---
+++ src/UnnamedClassesAndInstanceMethodExample.java
@@ -0,0 +1,4 @@
+void main(){
+    //This is a ex of Java 21
+    System.out.println("Hello this is example of Unnamed class & INstance method");
+}
